73a148db625e7dc7fa3c523ad217cd2d4f70c915
Benedictus Kent <benedictuskent123@gmail.com>
2022-10-12 16:41:44 +0800

    enemy movement + animation

--- Dooms Day/Assets/Scripts/EnemyMovement.cs
+++ Dooms Day/Assets/Scripts/EnemyMovement.cs
@@ -6,14 +6,23 @@ public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
 
     private Vector2 movement;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
