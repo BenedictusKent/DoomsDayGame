c93d47a2cbcac4e3d5e80bd63a5c566993419fcf
peter880630aaa <peter880630@yahoo.com.tw>
2022-10-18 18:28:29 +0800

    10/18 difficulty / index

--- Dooms Day/Assets/Scripts/MainMenu.cs
+++ Dooms Day/Assets/Scripts/MainMenu.cs
@@ -7,12 +7,38 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("SelectMenu");
     }
 
     public void ReturnMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("SelectMenu");
+    }
+
+    public void PlayGameEasy()
+    {
+        DataBase.AIattacktime = 1f;
+        DataBase.AImovetime = 0.75f;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayGameNormal()
+    {
+        DataBase.AIattacktime = 0.5f;
+        DataBase.AImovetime = 0.5f;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayGameHard()
+    {
+        DataBase.AIattacktime = 0.4f;
+        DataBase.AImovetime = 0.4f;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Again()
+    {
+        SceneManager.LoadScene("Game");
     }
 
     public void QuitGame()
